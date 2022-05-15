dfd94e3760aebf47215c925617ad331997a0e8d3
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-05-15 12:05:07 -0500

    Created all Pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from './Home';
+import Breakfast from './Breakfast';
+import Lunch from './Lunch';
+import Dinner from './Dinner'
+import Reviews from './Reviews'
+
+export {
+    Home,
+    Breakfast,
+    Lunch,
+    Dinner,
+    Reviews
+}
