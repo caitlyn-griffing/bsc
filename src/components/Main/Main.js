5cb0179dbe5a0015952e797a05da485d8acfa20c
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-05-16 14:11:29 -0500

    scss for Main Component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import './Main.scss'
+import old from '../ComponentImages/oldschool-pic-og.png'
 
 function Main() {
   return (
     <div className="main">
-      <h1>Main</h1>
       <div className="icon">
         <a href="https://www.facebook.com"><i class="fa-solid fa-thumbs-up fa-2xl"></i></a>
       </div>
-      <h3>Hours</h3>
-      <p>
-        Monday-Saturday 6am-2pm
-        <br />
-        Sunday 7am-2pm
-      </p>
-      <h3>Contact/Location</h3>
-      <p>
-        972-442-0453
-        <br />
-        112 N. Ballard Avenue, Wylie, TX 75098
-      </p>
-      <h3>About Them</h3>
-      <p>
-        If you’re looking for high quality and personal service, you’ve come to the right place. At Ballard Street Cafe we’ll give you the attention and personal service you’ll come to expect and enjoy. We offer the best in Home Cooking, Best Breakfast around and Best Chicken Fried Steak. Ballard Street Cafe has been located in Wylie since 1997. Let us feed you the best homecooking you've had since Grandma's house.
-      </p>
+      <div className="mainContentCtn">
+        <div className="mainInfo">
+          <div className="hoursCtn">
+            <h3 className="mainTitles">Hours</h3>
+            <hr className="hr" />
+            <p>
+              Monday-Saturday 6am-2pm
+              <br />
+              Sunday 7am-2pm
+            </p>
+          </div>
+          <div className="contactLocationCtn">
+            <h3 className="mainTitles">Contact/Location</h3>
+            <hr className="hr" />
+            <p>
+              972-442-0453
+              <br />
+              112 N. Ballard Avenue, Wylie, TX 75098
+            </p>
+          </div>
+        </div>
+        <div className="mainImage">
+          <img src={old} alt="Front of Ballard Street Cafe" />
+        </div>
+        <div className="aboutCtn">
+          <h3 className="mainTitles">About Us</h3>
+          <hr className="hr" />
+          <p>
+            If you’re looking for high quality and personal service, you’ve come to the right place. At Ballard Street Cafe we’ll give you the attention and personal service you’ll come to expect and enjoy. We offer the best in Home Cooking, Best Breakfast around and Best Chicken Fried Steak. Ballard Street Cafe has been located in Wylie since 1997. Let us feed you the best homecooking you've had since Grandma's house.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
