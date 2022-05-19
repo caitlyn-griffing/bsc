60625ac6ec8e0e75dd4fc7bf9430f8fa13d1afde
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-05-18 20:26:57 -0500

    added navbar paths

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import './Navbar.scss'
+import navbarItems from './NavbarItems'
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="navLink">
-        Home
-      </div>
-      <div className="navLink">
-        Breakfast
-      </div>
-      <div className="navLink">
-        Lunch
-      </div>
-      <div className="navLink">
-        Dinner
-      </div>
-      <div className="navLink">
-        Reviews
-      </div>
+    <div className="navbar" id="navbar">
+      <ul>
+        {
+          navbarItems.map(({ cName, src, name }) => {
+            // cName = 'navbar-item'
+            return(
+              <li className={cName}><a href={src}>{name}</a></li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
---
+++ src/components/Navbar/NavbarItems.js
@@ -0,0 +1,32 @@
+// Content for Navbar Items
+const cName = 'navbar-item'
+
+const navbarItems = [
+    {
+        name: 'Home',
+        src: '/',
+        cName
+    },
+    {
+        name: 'Breakfast',
+        src: '/breakfast',
+        cName
+    },
+    {
+        name: 'Lunch',
+        src: '/lunch',
+        cName
+    },
+    {
+        name: 'Dinner',
+        src: '/dinner',
+        cName
+    },
+    {
+        name: 'Reviews',
+        src: '/reviews',
+        cName
+    },
+]
+
+export default navbarItems;
